6fed4c09362560cec4896f31763f7e5091f366c2
Sheelavati H B <sheelahb8@gmail.com>
2024-09-25 12:00:27 +0530

    first C code

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int factorial(int n){
+	if(n<=1){
+		return 1;
+	}
+	else{
+		return n*factorial(n-1);
+	}
+}
+int main(){
+	int num = 5;
+	int res = factorial(5);
+	printf("Res of fact is:%d\n", res);
+	return 0;
+}
